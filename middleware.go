929e04606a6eb7619f0e0949c2bdc2a1688a2d25
Richard Imaoka <richard.e.imaoka@gmail.com>
2023-05-01 04:09:04 +0900

    simplify

--- middleware.go
+++ middleware.go
@@ -1,27 +1,17 @@
 package main
 
 import (
-	"database/sql"
+	"fmt"
 	"net/http"
 )
 
-// As it's difficult read the Middleware function in https://gqlgen.com/recipes/authentication/
-// I'm summarizing types used in the function.
-//
-// *sql.DB       -> |Middleware| -> Func
-//  http.Handler -> |   Func   | -> http.Handler
+// definition of a function in Func type
+func Middleware(h http.Handler) http.Handler {
+	// using http.HandlerFunc, you can create http.Handler from a function
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		authorization := r.Header.Get("Authorization")
+		fmt.Println("Authorization: ", authorization)
 
-type Func func(http.Handler) http.Handler
-
-func Middleware(db *sql.DB) Func {
-
-	// definition of a function in Func type
-	return func(next http.Handler) http.Handler {
-
-		// Func's return type is http.Handler
-		// using http.HandlerFunc, you can create http.Handler from a function
-		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			next.ServeHTTP(w, r)
-		})
-	}
+		h.ServeHTTP(w, r)
+	})
 }
