b08a390257a68951b2cf05a463655c852de7a4de
Richard Imaoka <richard.e.imaoka@gmail.com>
2023-05-01 04:11:47 +0900

    move middleware to server.go

--- server.go
+++ server.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 	"os"
@@ -12,6 +13,17 @@ import (
 
 const defaultPort = "8080"
 
+// receive http.Handler and return http.Handler
+func Middleware(h http.Handler) http.Handler {
+	// using http.HandlerFunc, you can create http.Handler from a function
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		authorization := r.Header.Get("Authorization")
+		fmt.Println("Authorization: ", authorization)
+
+		h.ServeHTTP(w, r)
+	})
+}
+
 func main() {
 	port := os.Getenv("PORT")
 	if port == "" {
@@ -20,7 +32,7 @@ func main() {
 
 	srv := handler.NewDefaultServer(graph.NewExecutableSchema(graph.Config{Resolvers: &graph.Resolver{}}))
 
-	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
+	http.Handle("/", Middleware(playground.Handler("GraphQL playground", "/query")))
 	http.Handle("/query", srv)
 
 	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
