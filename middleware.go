a234864d58a12d50458ee563ba59c628c6861286
Richard Imaoka <richard.e.imaoka@gmail.com>
2023-05-01 03:53:44 +0900

    barebone 1 level deeper

--- middleware.go
+++ middleware.go
@@ -8,7 +8,11 @@ import (
 // As it's difficult read the Middleware function in https://gqlgen.com/recipes/authentication/
 // I'm summarizing types used in the function.
 //
-// *sql.DB -> |Middleware| -> http.Handler
+// *sql.DB       -> |Middleware| -> Func
+//  http.Handler -> |   Func   | -> http.Handler
+
 func Middleware(db *sql.DB) func(http.Handler) http.Handler {
-	return nil
+	return func(next http.Handler) http.Handler {
+		return nil
+	}
 }
